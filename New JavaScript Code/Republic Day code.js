75720c66f6c01229e49ffe8ebf1c0a908d350a10
Ananda <ganesh.ananda1387@gmail.com>
2023-01-25 23:54:38 +0530

    added my own RD codes

---
+++ New JavaScript Code/Republic Day code.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewpoint" content="width=device-width">
+<title>My Javascript</title>
+<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<img width="300" height="175" src="https://cdn.britannica.com/97/1597-004-05816F4E/Flag-India.jpg" alt="India flag" /> 
+<h1><i> <p style="color:orange;">Republic Day</i></h1>
+<h2> <p style="color:green;">74th Republic Day Celebrations</h2>
+
+<hr>Hello Ananad, Happy Republic Day!</body</hr>
+<p>Introduction to Republic Day</p>
+<p><pre><blockquote>Republic Day marks the adoption of India's constitution and the country's transition to a republic on January 26, 1950. Every year, the celebrations marking the day feature spectacular military and cultural pageantry. In New Delhi, armed forces personnel march along the Kartavya path in an elaborate display of military might. The epic show on Kartavya path eclipses everything else happening across the country on this auspicious day.</pre></blockquote> </p></body
+<main>
+<p><b>Award Distribution</b></p>
+
+On the eve of <a href="https://www.google.com">Republic Day</a>, the President of India distributes Padma Awards to the civilians of India every year. These are the second highest civilian awards in India after Bharat Ratna. These awards are given in three categories, viz. Padma Vibhushan, Padma Bhushan and Padma Shri, in decreasing order of importance.[15].
+
+<p> &#x2022; <b>Padma Vibhushan</b> for "exceptional and distinguished service". Padma Vibhushan is the second-highest civilian award in India</p>
+<p> &#x2022; <b>Padma Bhushan</b> for "distinguished service of a high order". Padma Bhushan is the third-highest civilian award in India</p>
+<p> &#x2022; <b>Padma Shri</b> for "distinguished service". Padma Shri is the fourth-highest civilian award in India</p>
+<a href="page2.html" >Page2</a>
+<a href="page3.html" >Page3</a>
+</main>
+<ul>
+<li>Dr B R Ambedakar</li>
+<ul>
+  <li>Sardar Vallabhbhai Patel</li>
+</ul>
+<li>Subhas Chandra Bose</li>
+<ul>
+  <li>Bhagat Singh</li>
+</ul>
+</ul>
+
+<p id="demo"></p>
+<script>
+document.getElementById("demo").innerHTML = new Date();
+
+</script>
+</body>
+</html>
